878d088aea3b98b34e271838c3f8969946d260a1
Muhammad Shahmeer <muhammadshahmeeronline@gmail.com>
2022-02-07 00:13:08 +0500

    dynamic content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      students: [
+        {
+          name: "Shahmeer",
+          id: "135",
+        },
+        {
+          name: "Ali",
+          id: "129",
+        },
+        {
+          name: "Ibrahim",
+          id: "014",
+        },
+        {
+          name: "Abdullah",
+          id: "077",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.students.map((student) => (
+          <h1 key={student.id}>{student.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
